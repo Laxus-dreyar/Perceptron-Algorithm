ba841e3d426964be0a4cae419f9cf7287661e557
Mrinal <mrinalkhubchandani13@gmail.com>
2020-05-28 09:00:47 +0530

    dataset ploted

---
+++ make-plot.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+dbfile = open('dataset','rb')
+db = np.array(pickle.load(dbfile))
+
+X = db[:,:1]
+Y = db[:,1:2]
+label = db[:,2:3]
+
+X_pos = []
+Y_pos = []
+X_neg = []
+Y_neg = []
+
+for i in range(len(X)):
+    if label[i] == 0:
+        X_neg.append(X[i])
+        Y_neg.append(Y[i])
+    else:
+        X_pos.append(X[i])
+        Y_pos.append(Y[i])
+
+plt.plot(X_pos,Y_pos,'bo')
+plt.plot(X_neg,Y_neg,'ro')
+plt.plot([-2,2],[-2,2],'-k')
+plt.show()
